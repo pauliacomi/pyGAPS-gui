3820ff608915fbe85caa3f79f2822da6d16c6f87
Paul Iacomi <pauliacomi@users.noreply.github.com>
2020-02-08 20:47:43 +0100

    argparse for open folder/files

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import argparse
+import pathlib
 import sys
 
 from PySide2.QtCore import Qt
@@ -22,13 +24,30 @@ def exception_hook(exctype, value, traceback):
     sys.exit(1)
 
 
+def process_cl_args():
+    """Process the known arguments."""
+    parser = argparse.ArgumentParser(
+        description='Directly open isotherms.')
+    parser.add_argument(
+        '--file', '-f', action='store', nargs='+',
+        help="Open one or more isotherms.")
+    parser.add_argument(
+        '--folder', action='store',
+        help="Open a folder of isotherms.")
+
+    parsed_args, unparsed_args = parser.parse_known_args()
+    return parsed_args, unparsed_args
+
+
 def main():
     """Main app entrypoint."""
     # Set the exception hook to our wrapping function
     sys.excepthook = exception_hook
 
     # Create application
-    app = QApplication(sys.argv)
+    parsed_args, unparsed_args = process_cl_args()
+    qt_args = sys.argv[:1] + unparsed_args
+    app = QApplication(qt_args)
 
     # Create kernel
     # kernel_manager = QtInProcessKernelManager()
@@ -39,6 +58,16 @@ def main():
     # Create main window and show
     application = MainWindow(None)  # (kernel_manager)
     application.show()
+
+    print(parsed_args)
+    if parsed_args.file:
+        filepaths = [pathlib.Path(x) for x in parsed_args.file]
+        application.load_cont(filepaths)
+
+    elif parsed_args.folder:
+        folder = pathlib.Path(parsed_args.folder)
+        filepaths = [x for x in folder.iterdir() if not x.is_dir()]
+        application.load_cont(filepaths)
 
     # Execute
     sys.exit(app.exec_())
