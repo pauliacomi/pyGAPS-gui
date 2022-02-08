3334de5c3afd8d7ea8c6a3dc6f6eadefd72a8a67
Paul Iacomi <pauliacomi@users.noreply.github.com>
2022-02-08 19:51:32 +0100

    clearer logging hook

--- pygapsgui/utilities/log_hook.py
+++ pygapsgui/utilities/log_hook.py
@@ -4,7 +4,18 @@ logging.captureWarnings(True)
 from io import StringIO
 
 
+class LogFilter():
+    """Filter a log based on its level."""
+    def __init__(self, level):
+        self._level = level
+
+    def filter(self, logRecord):
+        """Perform filter on incoming log collection."""
+        return logRecord.levelno <= self._level
+
+
 class LogHook():
+    """Ties in to the pyGAPS logging functionality and temporarily captures all output."""
 
     log_stream = None
     logger = None
@@ -13,23 +24,41 @@ class LogHook():
     def __init__(self):
         self.log_stream = StringIO()
         self.logger = logging.getLogger('pygaps')
-        self.handler = logging.StreamHandler(stream=self.log_stream)
-        self.handler.setLevel(logging.INFO)
+        self.infohandler = logging.StreamHandler(stream=self.log_stream)
+        self.infohandler.setLevel(logging.INFO)
+        self.infohandler.addFilter(LogFilter(logging.INFO))
+        info_fmt = logging.Formatter("<font color=\"black\">%(message)s</font>")
+        self.infohandler.setFormatter(info_fmt)
+        self.warninghandler = logging.StreamHandler(stream=self.log_stream)
+        self.warninghandler.setLevel(logging.WARNING)
+        self.warninghandler.addFilter(LogFilter(logging.WARNING))
+        warning_fmt = logging.Formatter("<font color=\"magenta\">Warning: %(message)s</font>")
+        self.warninghandler.setFormatter(warning_fmt)
+        self.errorhandler = logging.StreamHandler(stream=self.log_stream)
+        self.errorhandler.setLevel(logging.ERROR)
+        self.errorhandler.addFilter(LogFilter(logging.ERROR))
+        error_fmt = logging.Formatter("<font color=\"red\">Error: %(message)s</font>")
+        self.errorhandler.setFormatter(error_fmt)
 
     def __enter__(self):
-        self.logger.addHandler(self.handler)
+        self.logger.addHandler(self.infohandler)
+        self.logger.addHandler(self.warninghandler)
+        self.logger.addHandler(self.errorhandler)
         return self
 
     def __exit__(self, type, value, traceback):
-        self.logger.removeHandler(self.handler)
+        self.logger.removeHandler(self.infohandler)
+        self.logger.removeHandler(self.warninghandler)
+        self.logger.removeHandler(self.errorhandler)
         return True
 
-    def getLogs(self):
+    def get_logs(self):
+        """Get all logs during the capture as a string."""
         logs = self.log_stream.getvalue().replace("\n", "<br>")
         self.log_stream.truncate(0)
         self.log_stream.seek(0)
         if logs:
-            return f'<font color="magenta">Warning: {logs}</font>'
+            return logs
         else:
             return ''
 
